836a82cb5ff27cdedb28fbdd9a5ab57d31eda980
mattgrayy <Matt2.Gray@live.uwe.ac.uk>
2016-10-19 15:01:49 +0100

    generic initial push

---
+++ Freedom/Assets/Scripts/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update (){
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * 5, GetComponent<Rigidbody2D>().velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0,250));
+        }
+	}
+}
