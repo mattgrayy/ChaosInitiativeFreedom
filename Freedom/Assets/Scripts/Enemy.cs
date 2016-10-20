6e4b7568d2198847fd71e2d0a1dc122f7417dcd9
Tredwood <tom.redwood@live.co.uk>
2016-10-20 12:46:45 +0100

    Enemy Scripts added
    see above

---
+++ Freedom/Assets/Scripts/Enemy.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    Rigidbody2D rb2D;
+    public Vector2 moveSpeed;
+    public GameObject target, needle;
+    private float attackTime;
+
+	void Start ()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        //target = GetComponent<>();
+    }
+	
+	void Update ()
+    {
+        if (target.transform.position.x < transform.position.x)
+        {
+            rb2D.MovePosition(rb2D.position - moveSpeed * Time.fixedDeltaTime); 
+        }
+        else if (target.transform.position.x > transform.position.x)
+        { rb2D.MovePosition(rb2D.position + moveSpeed * Time.fixedDeltaTime); }
+
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "needle")
+        {
+            //change to saved sprite
+        }
+        if (coll.gameObject.tag == "player")
+        {
+            attackTime = Time.time;
+        }
+    }
+
+    void OncollisionDuration2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "player")
+        {
+            if (Time.time - attackTime > 0.5)
+            {
+                Debug.Log("Attack Player");
+            }
+
+        }
+    }
+}
