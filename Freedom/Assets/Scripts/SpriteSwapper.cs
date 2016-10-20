d0d417c70f6c5aabe4c73a58278535c3869c68df
mattgrayy <Matt2.Gray@live.uwe.ac.uk>
2016-10-20 12:55:10 +0100

    weapon swinging and sprite swapping test

---
+++ Freedom/Assets/Scripts/SpriteSwapper.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SpriteSwapper : MonoBehaviour {
+
+    [SerializeField] List<SpriteRenderer> sprites;
+
+    int current = 0;
+    int next = 0;
+    bool swapping = false;
+
+    public void swap(int _next)
+    {
+        if (!swapping)
+        {
+            swapping = true;
+            next = _next;
+        }
+        else
+        {
+            Color tmp = sprites[current].color;
+            tmp.a = 0;
+            sprites[current].color = tmp;
+
+            tmp = sprites[next].color;
+            tmp.a = 1;
+            sprites[next].color = tmp;
+            current = next;
+
+            swapping = true;
+            next = _next;
+        }
+    }
+
+	void Update ()
+    {
+        if (swapping)
+        {
+            if (sprites[current].color.a > 0)
+            {
+                Color tmp = sprites[current].color;
+                tmp.a -= 0.05f;
+                sprites[current].color = tmp;
+
+                tmp = sprites[next].color;
+                tmp.a += 0.05f;
+                sprites[next].color = tmp;
+            }
+            else
+            {
+                swapping = false;
+                current = next;
+            }
+        }
+	}
+}
