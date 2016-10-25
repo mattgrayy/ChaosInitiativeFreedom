59234f312fe4b5959861104f53ba36c6b5f6cc48
OwenRuddle <Owen2.Ruddle@live.uwe.ac.uk>
2016-10-25 21:15:38 +0100

    added BFT to game

--- Freedom/Assets/Scripts/CutSceneScript.cs
+++ Freedom/Assets/Scripts/CutSceneScript.cs
@@ -5,64 +5,71 @@ using UnityEngine.UI;
 
 
 
-public class CutSceneScript : MonoBehaviour {
+public class CutSceneScript : MonoBehaviour
+{
 
-	public int SequencePos;
-	public List<SpriteRenderer> ImageList;
+    public int SequencePos;
+    public List<SpriteRenderer> ImageList;
 
-	public Text title;
-	public Button startButton;
-
-
-
-	IEnumerator Fade()
-	{
-		for (float f = 1f; f >= 0f; f -= 0.01f) {
-			Color alfha = ImageList [SequencePos].color;
-			alfha.a = f;
-			ImageList [SequencePos].color = alfha;
-
-		}
-		yield return null;
-	}
-
-
-	void Start()
-	{
-		SequencePos = 0;
-
-	}
-
-	
-	// Update is called once per frame
-	void Update () {
-
-		if ((Input.GetKeyDown (KeyCode.Space)) && (SequencePos!= 3)  && (SequencePos!= 4)) {
-			StartCoroutine ("Fade");
-			SequencePos++;
-
-		}
-
-
-		if (SequencePos == 3) {
-			Debug.Log ("gfjgfjsdgf");
-
-		if (SequencePos == 7) {
-
-			title.gameObject.SetActive (true);
-			startButton.gameObject.SetActive (true);
-			SequencePos++;
-		}
-
-		//display images
-		//if i==7
-		//do zoom thing
-
-		//if key press then i++
+    public Text title;
+    public Button startButton;
 
 
 
+    IEnumerator Fade()
+    {
+        for (float f = 1f; f >= 0f; f -= 0.01f)
+        {
+            Color alfha = ImageList[SequencePos].color;
+            alfha.a = f;
+            ImageList[SequencePos].color = alfha;
 
-	
-	}
+        }
+        yield return null;
+    }
+
+
+    void Start()
+    {
+        SequencePos = 0;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if ((Input.GetKeyDown(KeyCode.Space)) && (SequencePos != 3) && (SequencePos != 4))
+        {
+            StartCoroutine("Fade");
+            SequencePos++;
+
+        }
+
+
+        if (SequencePos == 3)
+        {
+            Debug.Log("gfjgfjsdgf");
+
+            if (SequencePos == 7)
+            {
+
+                title.gameObject.SetActive(true);
+                startButton.gameObject.SetActive(true);
+                SequencePos++;
+            }
+
+            //display images
+            //if i==7
+            //do zoom thing
+
+            //if key press then i++
+
+
+
+
+
+        }
+    }
 }
