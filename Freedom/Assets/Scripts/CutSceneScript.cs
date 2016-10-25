99e69af9dfde4fe169c0befe8421b438ee4182ca
Dreggrim <Jake2.King@live.uwe.ac.uk>
2016-10-25 20:56:08 +0100

    start sounds setup
    soughn

--- Freedom/Assets/Scripts/CutSceneScript.cs
+++ Freedom/Assets/Scripts/CutSceneScript.cs
@@ -5,64 +5,93 @@ using UnityEngine.UI;
 
 
 
-public class CutSceneScript : MonoBehaviour {
+public class CutSceneScript : MonoBehaviour
+{
 
-	public int SequencePos;
-	public List<SpriteRenderer> ImageList;
+    public int SequencePos;
+    public List<SpriteRenderer> ImageList;
 
-	public Text title;
-	public Button startButton;
-
-
-
-	IEnumerator Fade()
-	{
-		for (float f = 1f; f >= 0f; f -= 0.01f) {
-			Color alfha = ImageList [SequencePos].color;
-			alfha.a = f;
-			ImageList [SequencePos].color = alfha;
-
-		}
-		yield return null;
-	}
+    public Text title;
+    public Button startButton;
 
 
-	void Start()
-	{
-		SequencePos = 0;
-
-	}
-
-	
-	// Update is called once per frame
-	void Update () {
-
-		if ((Input.GetKeyDown (KeyCode.Space)) && (SequencePos!= 3)  && (SequencePos!= 4)) {
-			StartCoroutine ("Fade");
-			SequencePos++;
-
-		}
+	public AudioSource Walking;
+	public AudioSource Sniffing;
+	public AudioSource TVclick;
+	public AudioSource TVsound;
 
 
-		if (SequencePos == 3) {
-			Debug.Log ("gfjgfjsdgf");
 
-		if (SequencePos == 7) {
 
-			title.gameObject.SetActive (true);
-			startButton.gameObject.SetActive (true);
-			SequencePos++;
+    IEnumerator Fade()
+    {
+
+
+        for (float f = 1f; f >= 0f; f -= 0.01f)
+        {
+            Color alfha = ImageList[SequencePos].color;
+            alfha.a = f;
+            ImageList[SequencePos].color = alfha;
+
+        }
+
+		switch (SequencePos) {
+		case 0:
+			TVclick.Play ();
+			break;
+		case 1:
+			Sniffing.Play ();
+			break;
 		}
 
-		//display images
-		//if i==7
-		//do zoom thing
-
-		//if key press then i++
 
 
+        yield return null;
+    }
 
 
-	
-	}
+    void Start()
+    {
+        SequencePos = 0;
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+		
+
+
+
+        if ((Input.GetKeyDown(KeyCode.Space)) && (SequencePos != 3) && (SequencePos != 4))
+        {
+			
+            StartCoroutine("Fade");
+            SequencePos++;
+
+        }
+
+		    if (SequencePos == 3)
+            {
+				TVsound.mute = true;
+				Walking.Play ();
+				
+                title.gameObject.SetActive(true);
+                startButton.gameObject.SetActive(true);
+                SequencePos++;
+            }
+
+            //display images
+            //if i==7
+            //do zoom thing
+
+            //if key press then i++
+
+
+
+
+
+      
+    }
 }
